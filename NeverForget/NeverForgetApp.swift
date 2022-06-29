2adf22e1d52ddc60326039db94bb48ae4f28a308
Carson <carsongro@gmail.com>
2022-06-29 13:14:46 -0600

    Initial Commit

---
+++ NeverForget/NeverForgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeverForgetApp.swift
+//  NeverForget
+//
+//  Created by Carson Gross on 6/29/22.
+//
+
+import SwiftUI
+
+@main
+struct NeverForgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
